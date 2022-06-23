bf7c940186b87da4f5671c7d19c7658846fb5aee
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-06-23 11:27:55 +0200

    todo style, buttons, conditional classes

--- src/components/TodoOne.vue
+++ src/components/TodoOne.vue
@@ -1,14 +1,153 @@
 <script setup lang="ts">
 import type Todo from "@/types/todo";
+import { computed } from "vue";
 // import { defineProps } from 'vue'
 
 const props = defineProps<{
   todo: Todo;
 }>();
+
+const userName = (userId: number): string => {
+  switch (userId) {
+    case 1:
+      return "Jake";
+    case 2:
+      return "Judy";
+    default:
+      return "Unknown";
+  }
+};
+
+const completedTag = computed((): string => {
+  return props.todo.completed ? "todo-tag--completed" : "";
+});
+
+const completeTodo = () => {
+  if (!props.todo.completed) {
+    props.todo.completed = true;
+  } else {
+    props.todo.completed = false;
+  }
+};
+
+const editTodo = (event: Event) => {
+  console.log(event.target);
+};
+const removeTodo = (event: Event) => {
+  console.log(event.target);
+};
 </script>
 
 <template>
-  <p>{{ props.todo.title }}</p>
+  <div class="todo">
+    <div class="todo-tag" :class="completedTag"></div>
+    <div class="todo-content">
+      <div class="todo-content-title">
+        {{ props.todo.title }}
+      </div>
+      <div class="todo-content-details">
+        <div class="todo-content-details__completed">
+          {{ props.todo.completed ? "Completed" : "Pending" }}
+        </div>
+        <div class="todo-content-details__user">
+          {{ userName(props.todo.userId) }}
+        </div>
+      </div>
+    </div>
+    <div class="todo-buttons">
+      <button class="btn btn-complete" @click="completeTodo">V</button>
+      <button class="btn btn-edit" @click="editTodo">E</button>
+      <button class="btn btn-trash" @click="removeTodo">X</button>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.todo {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  min-width: 400px;
+  max-width: 700px;
+  width: 100%;
+  border-radius: 5px;
+  border-color: #cfcfcf;
+  color: #cfcfcf;
+  background-color: #3b3939;
+  margin-bottom: 15px;
+
+  &-tag {
+    width: 10px;
+    border-radius: 5px 0 0 5px;
+    background-color: #2d7fa8;
+    margin-right: 25px;
+
+    &--completed {
+      background-color: $primary-green;
+    }
+  }
+
+  &-buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 12px;
+
+    .btn {
+      width: 33px;
+      height: 33px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      border-radius: 5px;
+      margin-left: 5px;
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+
+    .btn-complete {
+      background-color: $primary-green;
+      color: #cfcfcf;
+    }
+
+    .btn-edit {
+      background-color: #2d7fa8;
+      color: #cfcfcf;
+    }
+
+    .btn-trash {
+      background-color: #a82d2d;
+      color: #cfcfcf;
+    }
+  }
+
+  &-content {
+    width: 100%;
+
+    &-title {
+      width: 100%;
+      font: 1.2em;
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+
+    &-details {
+      width: 100%;
+      display: flex;
+      align-self: flex-end;
+
+      &__completed {
+        flex-grow: 1;
+      }
+
+      &__user {
+        flex-grow: 1;
+        text-align: right;
+      }
+    }
+  }
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,20 +3,39 @@ import { onMounted, ref, computed } from "vue";
 import { getTodos } from "@/services/api/todosApi";
 import type Todo from "@/types/todo";
 import TodoOne from "@/components/TodoOne.vue";
-// import BaseButton from "@/components/base/BaseButton.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
 
 let todos = ref<Todo[]>([
   {
     userId: 1,
     id: 1,
     title: "sample todo",
-           completed: false,
+    completed: false,
   },
 ]);
 
-const selectedTodos = computed(() => {
-  return todos.value.slice(0, 9);
-});
+let localTodos = ref<any[] | Todo[]>([]);
+
+let todoInput = ref<string>("");
+
+//const selectedTodos = computed((): Todo[] => {
+//  return todos.value.slice(0, 9);
+//});
+
+//const completedTodos = computed((): Todo[] => {
+//  return selectedTodos.filter((todo: Todo) => todo.completed);
+//});
+
+const addTodo = () => {
+  let newTodo: Todo = {
+    userId: 1,
+    id: Math.floor(Math.random() * 11111),
+    title: todoInput.value,
+    completed: false,
+  };
+  localTodos.value.push(newTodo);
+  todoInput.value = ''
+};
 
 onMounted(async () => {
   const data = await getTodos();
@@ -25,23 +44,52 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="todos">
-    <TodoOne v-for="todo in selectedTodos" :key="todo.id" :todo="todo"/>
+  <div class="page-wrapper">
+    <div class="add-todo">
+      <input
+        type="text"
+        class="add-todo__input"
+        v-model="todoInput"
+        @keyup.enter="addTodo"
+      />
+      <BaseButton @click="addTodo">Add Todo</BaseButton>
+    </div>
+    <div class="todos">
+      <TodoOne v-for="todo in localTodos" :key="todo.id" :todo="todo" />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.todos {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-
-  &--buttons {
-    padding-top: 2em;
-    width: 33%;
+.page-wrapper {
+  .add-todo {
     display: flex;
-    justify-content: space-evenly;
+    min-width: 400px;
+    max-width: 700px;
+    width: 100%;
+    margin-bottom: 40px;
+
+    & input {
+      flex-grow: 4;
+      margin-right: 10px;
+      border: none;
+      border-radius: 5px;
+      padding: 0 10px;
+    }
+  }
+
+  .todos {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+
+    &--buttons {
+      padding-top: 2em;
+      width: 33%;
+      display: flex;
+      justify-content: space-evenly;
+    }
   }
 }
 </style>
